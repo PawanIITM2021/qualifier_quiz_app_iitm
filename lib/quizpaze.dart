33dd87d248821e074b57f258699201c35aed6b40
PawanIITM2021 <pawanpathak893@gmail.com>
2021-12-25 20:08:19 +0530

    modified..still_Working.......

--- lib/quizpaze.dart
+++ lib/quizpaze.dart
@@ -33,8 +33,88 @@ class quizpage extends StatefulWidget {
 }
 
 class _quizpageState extends State<quizpage> {
+  Widget choicebutton() {
+    return Padding(
+      padding: EdgeInsets.symmetric(
+        vertical: 10.0,
+        horizontal: 20.0,
+      ),
+      child: MaterialButton(
+        onPressed: () {},
+        child: Text(
+          "Option 1",
+          style: TextStyle(
+            color: Colors.white,
+            fontFamily: "Alike",
+            fontSize: 16.0,
+          ),
+        ),
+        color: Colors.indigoAccent,
+        splashColor: Colors.indigoAccent[700],
+        highlightColor: Colors.indigoAccent[700],
+        minWidth: 200.0,
+        height: 45.0,
+        shape:
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            flex: 3,
+            child: Container(
+              padding: EdgeInsets.all(15.0),
+              alignment: Alignment.bottomLeft,
+              child: Text(
+                "This is a sample Question which will be displayed ?",
+                style: TextStyle(
+                  fontSize: 18.0,
+                  fontFamily: "Quando",
+                ),
+              ),
+              decoration: BoxDecoration(
+                color: Colors.orangeAccent,
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 6,
+            child: Container(
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    choicebutton(),
+                    choicebutton(),
+                    choicebutton(),
+                    choicebutton(),
+                  ]),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Container(
+              child: Center(
+                child: Text(
+                  "30",
+                  style: TextStyle(
+                    fontSize: 35.0,
+                    fontWeight: FontWeight.w700,
+                    fontFamily: 'Times New Roman',
+                  ),
+                ),
+              ),
+              decoration: BoxDecoration(
+                color: Colors.tealAccent,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
